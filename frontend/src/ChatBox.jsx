da73542ccb4251751b88d0e3bc0a83b069fad0f1
Mikhail Stepanov <energetikcity@yandex.ru>
2025-05-10 16:12:52 +0300

    sync update 2025-05-10 16:12:52

--- frontend/src/ChatBox.jsx
+++ frontend/src/ChatBox.jsx
@@ -14,7 +14,7 @@ export default function ChatBox() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/chat", {
+      const res = await fetch("http://172.31.168.247:8000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt: input }),
